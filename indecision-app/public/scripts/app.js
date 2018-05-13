decbbbc250dec3be276ea84e4f93c47c4eff438b
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-13 20:21:29 -0300

    Manual Data Binding

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,122 +1,55 @@
 'use strict';
 
-console.log('App.js is running!');
-
-var app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
-};
-
-// JSX - JavaScript XML
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options' : 'No options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item one'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item two'
-    )
-  )
-);
-
-var user = {
-  name: 'Luís F. Bilecki',
-  age: 24,
-  location: 'Rio Negrinho, SC'
-};
-
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      location
-    );
-  }
-}
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name ? user.name : 'Anonymous'
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
-
 var count = 0;
 
 var addOne = function addOne() {
   console.log('addOne');
+  count++;
+  renderCounterApp();
 };
 
 var minusOne = function minusOne() {
   console.log('minusOne');
+  count--;
+  renderCounterApp();
 };
 
 var reset = function reset() {
   console.log('reset');
+  count = 0;
+  renderCounterApp();
 };
-
-var templateThree = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Count: ',
-    count
-  ),
-  React.createElement(
-    'button',
-    { onClick: addOne },
-    '+1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: minusOne },
-    '-1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: reset },
-    'reset'
-  )
-);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateThree, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateThree = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'reset'
+    )
+  );
+
+  ReactDOM.render(templateThree, appRoot);
+};
+
+renderCounterApp();
