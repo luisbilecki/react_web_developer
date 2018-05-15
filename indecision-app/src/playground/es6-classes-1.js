f5d787617cf73931d95f3ddf5eee32cd14b96a74
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-15 20:01:56 -0300

    ES6 Classes - Part II

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -4,7 +4,7 @@ class Person {
     this.age = age;
   }
 
-  getGretting() {
+  getGreeting() {
    // return 'Hi, I am ' + this.name + '!';
    return `Hi, I am ${ this.name }!`;
   }
@@ -17,10 +17,55 @@ class Person {
   }
 }
 
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+
+    this.major = major;
+  }
+
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.homeLocation) {
+      greeting += ` I am visiting from ${this.homeLocation}.`;
+    }
+
+    return greeting;
+  }
+}
+
 const me = new Person('Luis', 24);
-console.log(me.getGretting());
+console.log(me.getGreeting());
 console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getGretting());
-console.log(me.getDescription());
+const other = new Traveler(undefined, undefined, 'Nowhere');
+console.log(other.getGreeting());
+
+const meStudent = new Student('Felipão', 24, undefined);
+console.log(meStudent.getGreeting());
+console.log(meStudent.getDescription());
+
+const meTraveler = new Traveler('Felipão', 24, 'New York, NY');
+console.log(meTraveler.getGreeting());
