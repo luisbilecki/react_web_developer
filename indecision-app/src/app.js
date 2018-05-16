497e40e8fb092bd7e1e556deea6b5fc026ce1e71
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-15 22:27:24 -0300

    Nesting components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -39,16 +52,24 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option component here
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(jsx, document.getElementById('app'));
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        AddOption component here
+      </div>
+    );
+  }
+}
 
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
