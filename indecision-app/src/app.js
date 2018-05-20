cdcabaa3cc55abf3e1c0e0184f935c8c01d6c488
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-19 23:10:18 -0300

    One component per file

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import validator from 'validator';
+import IndecisionApp from './components/IndecisionApp';
 
-console.log(validator.isEmail('test@gmail.com'));
-
-const template = <p>THIS IS JSX FROM WEBPACK</p>;
-
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
