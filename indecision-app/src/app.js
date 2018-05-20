fee0d1a0f8312d64660089417d0be140cd37553e
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-19 23:03:52 -0300

    Setting up babel with Webpack

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,6 +4,6 @@ import validator from 'validator';
 
 console.log(validator.isEmail('test@gmail.com'));
 
-const template = React.createElement('p', {}, 'testing 123');
+const template = <p>THIS IS JSX FROM WEBPACK</p>;
 
 ReactDOM.render(template, document.getElementById('app'));
