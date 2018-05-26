793c50bb898a2954b7f54c0144b7a1c9ef12ca53
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-26 17:00:49 -0300

    Passing children to component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,30 +3,3 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-class OldSyntax {
-  constructor() {
-    this.name = "LUIS";
-    this.getGreeting = this.getGreeting().bind(this);
-  }
-
-  getGreeting() {
-    return `Hi. My name is ${this.name}`;  
-  }
-}
-
-const oldSyntax = new OldSyntax();
-console.log(oldSyntax.getGreeting());
-
-// ----
-
-class NewSyntax {
-  name = 'Felipe'
-
-  newGetGreeting = () => {
-    return `Hi. My name is ${this.name}`;  
-  }
-}
-
-const newSyntax = new NewSyntax();
-console.log(newSyntax.newGetGreeting());
