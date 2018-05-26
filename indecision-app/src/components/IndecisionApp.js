c5397673a80271b57d4216bfc90a909f323e85ff
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-26 19:38:21 -0300

    Theming with variables

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -74,7 +74,7 @@ export default class IndecisionApp extends React.Component {
     const subtitle = 'Put your life in the hands of a computer';
 
     return (
-      <div>
+      <div className="container">
         <Header subtitle={subtitle} />
         <Action
           hasOptions={this.state.options.length > 0}
