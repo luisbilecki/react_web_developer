4e18fda005cde99dc64010e79217efa30d247cc8
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-06-15 23:29:41 -0300

    Query string and params in url

--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => (
     <div className="menu">
       <NavLink to="/" activeClassName="is-active" exact>Dash</NavLink>
       <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-      <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
       <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </div>
   </header>
