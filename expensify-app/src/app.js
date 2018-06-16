c5ac0bd0ceaf6f87a37325b47c93c26e7174c609
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-06-15 22:10:00 -0300

    Add react-router-dom in expensify project

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+const AddExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard add expense component
+  </div>
+);
+
+const EditExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard edit expense component
+  </div>
+);
+
+const HelpExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard help expense component
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={ ExpenseDashboardPage } />
+      <Route exact path="/create" component={ AddExpenseDashboardPage } />
+      <Route exact path="/edit" component={ EditExpenseDashboardPage } />
+      <Route exact path="/help" component={ HelpExpenseDashboardPage } />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
