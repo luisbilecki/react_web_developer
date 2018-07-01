ca81aa6ea7366a3f1b140ebbf527bebbcb99f178
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-07-01 18:28:05 -0300

    ES6 Array Destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,4 @@
+// Object Destructuring
 /*
 const person = {
     age: 24,
@@ -19,7 +20,6 @@ const { city, temp:temperature } = person.location;
 if (temperature && city) {
     console.log(`It's ${temperature}°C in ${city}.`);
 }
-*/
 
 const book = {
     title: 'The Da Vinci Code',
@@ -32,3 +32,19 @@ const book = {
 const { name: publisherName = 'Self-Published' } = book.publisher
 
 console.log(publisherName);
+*/
+
+//
+// Array destructuring
+//
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+
+const [ , city, state = 'New York'] = address;
+
+console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, ,mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
