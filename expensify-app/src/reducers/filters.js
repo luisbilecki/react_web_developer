334c630faa6f5975f94a18590d4c49a026efe9d4
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-08-04 22:46:57 -0300

    React with Redux - Filtering by dates

--- expensify-app/src/reducers/filters.js
+++ expensify-app/src/reducers/filters.js
@@ -1,39 +1,41 @@
+import moment from 'moment';
+
 // Filters Reducer
 const filtersReducerDefaultState = {
-    text: '',
-    sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined
+  text: '',
+  sortBy: 'date',
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 };
-  
+
 export default (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
     case 'SET_TEXT_FILTER':
-    return {
+      return {
         ...state,
         text: action.text
-    };
+      };
     case 'SORT_BY_AMOUNT':
-    return {
+      return {
         ...state,
         sortBy: 'amount'
-    };
+      };
     case 'SORT_BY_DATE':
-    return {
+      return {
         ...state,
         sortBy: 'date'
-    };
+      };
     case 'SET_START_DATE':
-    return {
+      return {
         ...state,
         startDate: action.startDate
-    };
+      };
     case 'SET_END_DATE':
-    return {
+      return {
         ...state,
         endDate: action.endDate
-    };
+      };
     default:
-    return state;
+      return state;
   }
 };
