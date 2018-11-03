82368725ad9f45db01f49bd7ae105f68ad62cf99
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-11-03 14:42:38 -0300

    Removing data from firebase

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -33,12 +33,32 @@ database.ref().set({
 //database.ref('location/city').set('Joinville');
 
 database.ref('attributes').set({
-  height: 173,
+  height: 175,
   weight: 69
 }).then(() => {
   console.log('Second set call worked.');
 }).catch((e) => {
   console.log('Things didnt for the second error', e);
 });
+
+database
+  .ref('attributes')
+  .remove()
+  .then(() => {
+    console.log('Attributes remove from database');
+  }).catch((e) => {
+    console.log('Did not remove data', e);
+  });
+
+/*
+database
+  .ref()
+  .remove()
+  .then(() => {
+    console.log('Data was removed');
+  }).catch((e) => {
+    console.log('Did not remove data', e);
+  });
+*/
 
 console.log('I made a request to change the data.');
