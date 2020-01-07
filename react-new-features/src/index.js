b5735a648519bdae48f29c2b0840397e320cc43e
Luis F. Bilecki <luis.bilecki@gmail.com>
2020-01-07 10:08:02 -0300

    191. useState vs. setState

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -6,22 +6,44 @@ import * as serviceWorker from './serviceWorker';
 // { count: 0, name: '' }
 const App = (props) => {
   const [count, setCount] = useState(props.count);
-
+  const [text, setText] = useState('');
+  
   return (
     <div>
-      <p>The current count is {count}</p>
+      <p>The current {text || 'count'} is {count}</p>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(0)}>reset</button>
       <button onClick={() => setCount(count + 1)}>+1</button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
     </div>
   )
 };
+
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count);
+//   const [text, setText] = useState('');
+
+//   const [state, setState] = useState({
+//     count: props.count,
+//     text: '',
+//   });
+  
+//   return (
+//     <div>
+//       <p>The current {state.text || 'count'} is {state.count}</p>
+//       <button onClick={() => setState({ count: state.count - 1})}>-1</button>
+//       <button onClick={() => setState({ count: 0 })}>reset</button>
+//       <button onClick={() => setState({ count: state.count + 1 })}>+1</button>
+//       <input value={state.text} onChange={(e) => setState({ text: e.target.value })} />
+//     </div>
+//   )
+// };
 
 App.defaultProps = {
   count: 0,
 }
 
-ReactDOM.render(<App initialCount={10} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
