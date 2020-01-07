53fdb7c6c37debcb241646ce8dae3105768023a0
Luis F. Bilecki <luis.bilecki@gmail.com>
2020-01-07 10:46:13 -0300

    193. The useEffect Hook

--- react-new-features/src/App.js
+++ react-new-features/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = (props) => {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState('');
+  
+  useEffect(() => {
+    console.log('useEffect ran');
+    document.title = count;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>The current {text || 'count'} is {count}</p>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
     </div>
-  );
-}
+  )
+};
+
+App.defaultProps = {
+  count: 0,
+};
 
 export default App;
---
+++ react-new-features/src/Notes.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+const Notes = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  })
+
+  const addNote = (e) => {
+    e.preventDefault();
+
+    setNotes([
+      ...notes,
+      { title, body }
+    ]);
+    setTitle('');
+    setBody('');
+  };
+
+  const removeNote = (idToRemove) => {
+    const newNotes = notes.filter((_, index) => index !== idToRemove);
+    
+    setNotes(newNotes);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {
+        notes.map((note, idx) => (
+            <div key={idx}>
+              <h3>{note.title}</h3>
+              <p>{note.body}</p>
+              <button onClick={() => removeNote(idx)}>&times;</button>
+            </div>
+        ))
+      }
+
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
+        <button type="submit">Add note</button>
+      </form>
+    </div>
+  )
+};
+
+export default Notes;
